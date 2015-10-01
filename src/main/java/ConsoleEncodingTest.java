ff83467087e58e0e639ec1d99c46c6f73599219e
Some Code <somecodegithub@gmail.com>
2015-10-01 16:51:35 +0200

    more SystemInfo, encoding

--- src/main/java/ConsoleEncodingTest.java
+++ src/main/java/ConsoleEncodingTest.java
@@ -1,5 +1,5 @@
 /**
- * java console encoding is tedious (at least with windows).
+ * java console encoding is tedious (at least with windows with JDK7).
  *
  * some hints:
  * http://stackoverflow.com/questions/14030811/system-out-character-encoding
@@ -16,15 +16,17 @@
  * - System.console() might fix some things but is not (or cannot be) used
  */
 public class ConsoleEncodingTest {
+
     public static void main(String[] args) {
         System.out.println("file.encoding=" + System.getProperty("file.encoding"));
+        System.out.println("sun.stdout.encoding=" + System.getProperty("sun.stdout.encoding"));
         System.out.println("Hell\u00f6 \u00dcmlauts.");
         System.out.println("\u00c4 \u00e4 \u00d6 \u00f6 \u00dc \u00fc \u00df"); // Ä, ä, Ö, ö, Ü, ü, ß
         if (args.length > 0) {
-            System.out.println("args[0]=" + args[0]);
+            System.out.println("args[0]=" + args[0]); // test "round trip"
         }
+        System.out.println("arabic \u06ad \u06a7");  // non latin
 
-        System.console().printf("using System.console(): Hell\u00f6 \u00dcmlauts.");
-
+        // System.console().printf("using System.console(): Hell\u00f6 \u00dcmlauts.");
     }
 }
--- src/main/java/ufw/SystemInfo.java
+++ src/main/java/ufw/SystemInfo.java
@@ -8,7 +8,7 @@ public class SystemInfo {
     public static void show() {
         String os = System.getProperty("os.name") + " (" + System.getProperty("os.arch") + ", " + System.getProperty("os.version") + ")";
         String runtime = System.getProperty("java.runtime.name") + " (" + System.getProperty("java.runtime.version") + ")";
-        String vm = System.getProperty("java.vm.name") + " (" + System.getProperty("java.vm.version") + ")";
+        String vm = System.getProperty("java.vm.name") + " (" + System.getProperty("java.vm.version") + ", " + System.getProperty("java.vm.info") + ")";
         Log.info("os=" + os);
         Log.info("vm=" + vm);
         Log.info("rt=" + runtime);
