eaddd091d48008f38b41cb7caf7b0f47f554da82
Some Code <somecodegithub@gmail.com>
2017-09-24 17:06:29 +0200

    add sleeper to TimeResolutionTest

--- src/test/java/test/ufw/TimeResolutionTest.java
+++ src/test/java/test/ufw/TimeResolutionTest.java
@@ -28,6 +28,42 @@ public class TimeResolutionTest {
     }
 
     @Test
+    public void testCurrentSleeper() throws Exception {
+        // this is insane. the sleeper thread will increase timer precision on windows.
+        Runnable sleeper = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(Integer.MAX_VALUE);
+                }
+                catch (InterruptedException e) {
+                    Log.info("interrupted.");
+                }
+            }
+        };
+        Thread sleeperThread = new Thread(sleeper, "sleeper");
+        sleeperThread.start();
+        Thread.sleep(50); // wait a while
+
+        int runs = 200;
+        long total = 0;
+        for (int i = 0; i < runs; i++) {
+            long start = System.currentTimeMillis();
+            long end;
+            while (true) {
+                end = System.currentTimeMillis();
+                if (end != start) {
+                    break;
+                }
+            }
+            Assert.assertTrue(end > start);
+            total += end - start;
+        }
+        Log.info("average currentTimeMillis step: " + 1.0 * total / runs);
+        sleeperThread.interrupt();
+    }
+
+    @Test
     public void testTimeUTC() {
         int runs = 50;
         long total = 0;
