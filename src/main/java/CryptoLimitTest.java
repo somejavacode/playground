5e3988adfeced6e6cf0935025adbcbd917854bbf
Some Code <somecodegithub@gmail.com>
2017-12-21 12:00:46 +0100

    fix white space

--- src/main/java/CryptoLimitTest.java
+++ src/main/java/CryptoLimitTest.java
@@ -1,6 +1,6 @@
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec; 
+import javax.crypto.spec.SecretKeySpec;
 import java.security.spec.AlgorithmParameterSpec;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -8,34 +8,34 @@ import java.lang.reflect.Modifier;
 public class CryptoLimitTest {
 
     public static void main(String[] args) throws Exception {
-	if (args.length == 1) {
-	   fixOld();
-	}
-	if (args.length == 2) {
-	   fixNew();
-	}
-	int maxKeySize = Cipher.getMaxAllowedKeyLength("AES/CBC/NoPadding");
+        if (args.length == 1) {
+            fixOld();
+        }
+        if (args.length == 2) {
+            fixNew();
+        }
+        int maxKeySize = Cipher.getMaxAllowedKeyLength("AES/CBC/NoPadding");
         System.out.println("aes keySize=" + maxKeySize);
-        
-        testAes256();        
+
+        testAes256();
     }
 
     // new "feature" with JDK 8u151
     public static void fixNew() throws Exception {
-	java.security.Security.setProperty("crypto.policy", "unlimited");
+        java.security.Security.setProperty("crypto.policy", "unlimited");
     }
 
     // "HACK" fix java unlimited crypto.     
     // Note: this does not fix "getMaxAllowedKeyLength".
     public static void fixOld() throws Exception {
-	Field isRestricted = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
-	if (Modifier.isFinal(isRestricted.getModifiers())) {
-	    Field modifiers = Field.class.getDeclaredField("modifiers");
-	    modifiers.setAccessible(true);
-	    modifiers.setInt(isRestricted, isRestricted.getModifiers() & ~Modifier.FINAL);
-	}
-	isRestricted.setAccessible(true);
-	isRestricted.set(null, Boolean.FALSE);
+        Field isRestricted = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
+        if (Modifier.isFinal(isRestricted.getModifiers())) {
+            Field modifiers = Field.class.getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(isRestricted, isRestricted.getModifiers() & ~Modifier.FINAL);
+        }
+        isRestricted.setAccessible(true);
+        isRestricted.set(null, Boolean.FALSE);
     }
 
     // just do "any" 256bit AES operation (with pointless data)
@@ -46,7 +46,7 @@ public class CryptoLimitTest {
         SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
         AlgorithmParameterSpec parameterSpec = new IvParameterSpec(iv);
         Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec, parameterSpec); 
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, parameterSpec);
         cipher.doFinal(new byte[16]);
     }
 }
